ae7413c492bfbb5c8c40e674364d38f62d5d9453
Akshat Jain <97946554+akshatjainsk1234@users.noreply.github.com>
2024-12-11 18:56:02 +0000

    conversation

---
+++ 01basics/conversation.js
@@ -0,0 +1,29 @@
+let score="33abc"
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(typeof score)
+// console.log(valueInNumber)
+
+// 33="33"
+// "33abc"=nan
+// true=1
+// false=0
+// let loggedIn=true;
+
+// console.log(loggedIn)
+
+let loggedIn=1;
+let booleanloggedIn=Boolean(loggedIn)
+// console.log(booleanloggedIn)
+
+//1=true
+// 0=flase
+//""=false
+// akshat="true"
+// let somenumber=33
+// let stringNumber=String(somenumber)
+// console.log(typeof stringNumber)
+let somenumber="33"
+let number=Number(somenumber)
+console.log(typeof number)
