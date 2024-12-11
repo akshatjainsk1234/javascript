da52b5a38740b74adffafc39e76f8dea2a917766
Akshat Jain <97946554+akshatjainsk1234@users.noreply.github.com>
2024-12-11 19:21:53 +0000

    operator

---
+++ 01basics/operation.js
@@ -0,0 +1,30 @@
+let value=3
+let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/4)
+
+// basic
+
+let string1="hello"
+let string2="Akshat"
+
+let string=string1+ " " + string2
+// console.log(string)
+
+// console.log("1" + 2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// // console.log(1+2+"2")
+// console.log(+true)
+// console.log(true)
+// console.log(+"")
+
+let number=100;
+// number++;
+++number;
+console.log(number)
