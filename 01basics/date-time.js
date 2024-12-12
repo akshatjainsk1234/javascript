782f56a165853e430a3b4e028cc453afe2d4583d
Akshat Jain <97946554+akshatjainsk1234@users.noreply.github.com>
2024-12-12 09:14:31 +0000

    date-time-brief

---
+++ 01basics/date-time.js
@@ -0,0 +1,19 @@
+const mydate=new Date()
+// console.log(mydate)
+
+// console.log(mydate.toString())
+
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleString())
+// console.log(typeof mydate)
+
+const mycreateddate=new Date(2023,0,23)
+// console.log(mycreateddate.toDateString())
+
+const mytimestamp=Date.now()
+// console.log(mytimestamp)
+
+let newdate=new Date()
+console.log(newdate)
+console.log(newdate.getDay())
+console.log(newdate.getMonth())
