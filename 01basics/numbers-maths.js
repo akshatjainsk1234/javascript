05cee4c200cf56fd60270692c8c0a6f12827539b
Akshat Jain <97946554+akshatjainsk1234@users.noreply.github.com>
2024-12-12 08:57:56 +0000

    numbers-maths-brief

---
+++ 01basics/numbers-maths.js
@@ -0,0 +1,40 @@
+const score=400;
+// console.log(score)
+
+const number=new Number(100)
+// console.log(number)
+
+// console.log(number.toString())
+// console.log(typeof number)
+
+// tofixed
+
+console.log(number.toFixed(5))
+
+const othernumber=123.286
+// console.log(othernumber.toPrecision(5))
+
+const no=1000000
+
+// console.log(no.toLocaleString())
+
+// Maths is javascript library
+
+// console.log(Math)
+
+// abs value negative convert to positive
+// console.log(Math.abs(-4))
+
+// console.log(Math.round(4.6))
+
+// floor take lowest value
+
+// console.log(Math.floor(4.10))
+// console.log(Math.sqrt(25))
+
+// console.log(Math.random())
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
