81536d56b668464d39b685774a0de14ea56c5437
Akshat Jain <97946554+akshatjainsk1234@users.noreply.github.com>
2024-12-12 09:58:18 +0000

    array1-basics

---
+++ 02basics/array.js
@@ -0,0 +1,27 @@
+// console.log("array")
+
+const myarray=[0,1,2,3,4]
+// console.log(myarray)
+// console.log(myarray[0])
+
+const heros=["akshay","ritesh"]
+
+// array method
+
+// myarray.push(5)
+// myarray.pop()
+// myarray.unshift(6)
+// myarray.shift()
+// console.log(myarray.includes(1))
+// console.log(myarray.indexOf(4))
+
+
+// console.log(myarray)
+// console.log(myarray)
+
+// slice
+const ans=myarray.slice(1,3)
+console.log(ans)
+
+const ans2=myarray.splice(1,3)
+console.log(ans2)
