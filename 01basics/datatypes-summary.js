cbd707e279ee941a1e0a2dc1cecb329311b7e16e
Akshat Jain <97946554+akshatjainsk1234@users.noreply.github.com>
2024-12-12 07:34:27 +0000

    DATATYPE-SUMMARY

---
+++ 01basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+// primitive datatype
+// it is called call by value 
+// string
+// number
+// null
+// undefined
+// booleon
+// symbol-ues for unique value 
+// BIGINT- handle for big value and scientific value
+
+//Reference (non primitive datatype)
+// Array
+// Object
+// Functions
+const score=100
+const scoreValue=100.3
+const isloggedIn=false;
+const outsideTemp=null;
+let useremail;
+
+// // symbol code
+// const id=Symbol('123')
+// const anotherId=Symbol('123')
+
+// console.log(id===anotherId)
+// Array
+
+
+const arrays=["shakti","maan"]
+// object
+let obj={
+    name:"shakti",
+    email:"@gmail.com"
+}
+
+// function
+
+const myfunction= function(){
+
+}
