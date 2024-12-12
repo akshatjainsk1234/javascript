ca521d57ec129508c419d7d56022b331b594ba64
Akshat Jain <97946554+akshatjainsk1234@users.noreply.github.com>
2024-12-12 08:24:42 +0000

    string basics

---
+++ 01basics/string.js
@@ -0,0 +1,35 @@
+const name="  aksHAt  "
+const repoCount=22
+
+// const ans=name+repoCount
+// console.log(ans)
+
+
+// console.log(`my name is ${name } and value is ${repoCount}`)
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name[0]);
+
+// console.log(name.charAt(2))
+// console.log(name.indexOf('t'))
+
+// const newstring=name.substring(0,2)
+// console.log(newstring)
+
+
+// const newstring=name.slice(-6,2)
+// console.log(newstring)
+
+// trim - it remove spaces
+
+// const ans=name.trim()
+// console.log(ans.length)
+// console.log(name.length)
+
+// replace()
+
+// const ans=name.replace("jain")
+// console.log(name.replace('jain'))
+// console.log(name.includes('akshat'))
+console.log(name.split('-'))
