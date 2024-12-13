e65558e2c059fa454831ea275389e55febdfa956
Akshat Jain <97946554+akshatjainsk1234@users.noreply.github.com>
2024-12-13 09:35:29 +0000

    function-basics

---
+++ 03basicsf/func.js
@@ -0,0 +1,33 @@
+// console.log("hello function")
+// function name(){
+//     console.log("akshat55")
+// }
+// name()
+
+
+// function addtwo(num1,num2){
+//     console.log(num1+num2)
+// }
+// addtwo(2,2)
+
+// const addtwo=function(num1,num2){
+//     return num1+num2;
+
+// }
+// const ans=addtwo(2,5)
+// console.log(ans)
+
+// const iseven=function(num){
+//     return num%2===0
+// }
+// const ans=iseven(3)
+// console.log(ans)
+
+
+
+const addtwo=(num1,num2)=>{
+    return num1+num2
+
+}
+const ans=addtwo(2,4)
+console.log(ans)
