dde21bce59e21e784cc62f2360ff54b5dea53bc7
Akshat Jain <97946554+akshatjainsk1234@users.noreply.github.com>
2024-12-13 10:07:22 +0000

    global-local scope-basics

---
+++ 03basicsf/global-local.js
@@ -0,0 +1,29 @@
+// // console.log("hello global local js")
+// let a=10;
+// const b=20;
+// var c=30
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// undefined
+// console.log(a)
+// var a=10 
+// can not be initilized
+// console.log(b)
+// let b=20
+// console.log(c)
+// const c=30
+
+// {
+//     let a=10;   // reference error
+// }
+// console.log(a)
+
+
+// console.log(b)
+                         // Reference error
+// {
+//     const b=20
+// }
