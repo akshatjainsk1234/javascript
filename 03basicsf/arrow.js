4b08097aa86a50cc9dbcf316bdfac36f6b5a453c
Akshat Jain <97946554+akshatjainsk1234@users.noreply.github.com>
2024-12-15 00:56:03 +0000

    arrow-basics

---
+++ 03basicsf/arrow.js
@@ -0,0 +1,30 @@
+const user={
+    username:"akshat",
+    price:888,
+
+    welcomeMessage: function(){
+        // console.log(`${this.username},welcome user`)
+        // console.log(this)
+    }
+}
+
+// user.welcomeMessage()
+// user.username="sam"
+// user.welcomeMessage()
+
+// function chai(){
+//     let username="akshat"
+//     console.log(this.username)
+// }
+// chai()
+
+// // Arrow function syntax
+
+// const chai=()=>{
+//     let username="akshat"
+//     console.log(this.username)
+// }
+// chai()
+
+
+
